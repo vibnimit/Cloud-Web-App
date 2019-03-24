93430e1c538d9ab1e82eea0476fc66bf11fcdded
Akshay Bhandari <abhand10@asu.edu>
2019-03-23 17:35:51 -0700

    Parameterized autoscaler module

--- autoscaling_controller/src/main/java/autoscaling_model/autoscaling_controller/AutoscalingController.java
+++ autoscaling_controller/src/main/java/autoscaling_model/autoscaling_controller/AutoscalingController.java
@@ -8,6 +8,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.codec.binary.Base64;
 
 import com.amazonaws.regions.Regions;
@@ -19,6 +26,12 @@ import com.amazonaws.services.ec2.model.DescribeInstanceStatusRequest;
 import com.amazonaws.services.ec2.model.DescribeInstanceStatusResult;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupsRequest;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupsResult;
+import com.amazonaws.services.ec2.model.DescribeSubnetsRequest;
+import com.amazonaws.services.ec2.model.DescribeSubnetsResult;
+import com.amazonaws.services.ec2.model.DescribeVpcsRequest;
+import com.amazonaws.services.ec2.model.DescribeVpcsResult;
 import com.amazonaws.services.ec2.model.Filter;
 import com.amazonaws.services.ec2.model.IamInstanceProfileSpecification;
 import com.amazonaws.services.ec2.model.Instance;
@@ -26,7 +39,10 @@ import com.amazonaws.services.ec2.model.InstanceStatus;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.SecurityGroup;
+import com.amazonaws.services.ec2.model.Subnet;
 import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.simpleworkflow.flow.worker.SynchronousActivityTaskPoller;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
@@ -34,23 +50,116 @@ import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
 
 public class AutoscalingController 
 {
-	final static int total_instances_allowed = 19;
-	final static String requestQueueUrl = "https://sqs.us-west-1.amazonaws.com/411110494130/RequestQueue";
+	static int total_instances_allowed;
+	static String requestQueueUrl;
     static Map<String, String> recent_instance_status = new HashMap<String, String>();
+	static String appAMIID;
+	static String appInstanceType;
+	static String appSubnetID;
+	static String appSecurityGroups;
+	static String keyName;
+	static String iamInstanceRole;
+	static String clusterID;
+
+	static boolean isNotNullOrEmpty(String str){
+	    return (str != null && !str.isEmpty());
+	}
+
+	static private String getDefaultVPC() {
+		String vpcID = null;
+		final AmazonEC2 ec2Client = AmazonEC2ClientBuilder.defaultClient();
+		DescribeVpcsRequest dvr = new DescribeVpcsRequest();
+		List <Filter> vpcFilters = Arrays.asList(
+				new Filter("isDefault", Arrays.asList("true")));
+		dvr.setFilters(vpcFilters);
+		DescribeVpcsResult vpcResult = ec2Client.describeVpcs(dvr);
+		vpcID = vpcResult.getVpcs().get(0).getVpcId();
+		return vpcID;
+	}
+	
+	static private String getDefaultSubnet() {
+		String subnetId = null;
+		final AmazonEC2 ec2Client = AmazonEC2ClientBuilder.defaultClient();
+		String vpcID = getDefaultVPC();
+		DescribeSubnetsRequest dsr = new DescribeSubnetsRequest();
+		List<Filter> subnetFilters = Arrays.asList(
+				new Filter("vpc-id", Arrays.asList(vpcID)));
+		dsr.setFilters(subnetFilters);
+		DescribeSubnetsResult result = ec2Client.describeSubnets(dsr);
+		List <Subnet> subnets = result.getSubnets();
+		for(Subnet s : subnets) {
+			String[] subs = s.getAvailabilityZone().split("-");
+			if(subs[subs.length - 1].contains("a")) {
+				subnetId = s.getSubnetId();
+			}
+		}
+		return subnetId;
+	}
 	
 	public static void main( String[] args )
     {
+        Options options = new Options();
+
+        Option webAMIIDOpt = new Option("allowedInstances", true, "Number of allowed app instances (Default: 19)");
+        webAMIIDOpt.setRequired(false);
+        options.addOption(webAMIIDOpt);
+
+        Option requestQueueUrlOpt = new Option("requestQueueUrl", true, "Request queue URL");
+        requestQueueUrlOpt.setRequired(true);
+        options.addOption(requestQueueUrlOpt);
+
+        Option appAMIIDOpt = new Option("appAMIID", true, "AMI ID for appserver instance (Default: ami-0e355297545de2f82)");
+        appAMIIDOpt.setRequired(false);
+        options.addOption(appAMIIDOpt);
+
+        Option appInstanceTypeOpt = new Option("appInstanceType", true, "Instance type for appserver instance (Default: t2.micro)");
+        appInstanceTypeOpt.setRequired(false);
+        options.addOption(appInstanceTypeOpt);
+
+        Option appSubnetIDOpt = new Option("appSubnetID", true, "Subnet ID for appserver instance (Default: one of the default subnet in region)");
+        appSubnetIDOpt.setRequired(false);
+        options.addOption(appSubnetIDOpt);
+
+        Option appSecurityGroupsOpt = new Option("appSecurityGroups", true, "Comma seperated list of security groups for appserver instance (Default: default)");
+        appSecurityGroupsOpt.setRequired(false);
+        options.addOption(appSecurityGroupsOpt);
+        
+        Option keyNameOpt = new Option("keyName", true, "Key name for server instance");
+        keyNameOpt.setRequired(true);
+        options.addOption(keyNameOpt);
+
+        Option iamInstanceRoleOpt = new Option("iamInstanceRole", true, "Unique cluster identifier used to tag all the resources created under this process");
+        iamInstanceRoleOpt.setRequired(true);
+        options.addOption(iamInstanceRoleOpt);
+
+        Option clusterIdentifier = new Option("clusterIdentifier", true, "Unique cluster identifier used to tag all the resources created under this process");
+        clusterIdentifier.setRequired(true);
+        options.addOption(clusterIdentifier);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("Bootstrap", options);
+
+            System.exit(1);
+        }
+        
+        total_instances_allowed = isNotNullOrEmpty(cmd.getOptionValue("allowedInstances")) ? Integer.parseInt(cmd.getOptionValue("allowedInstances")) : 19;
+        requestQueueUrl = cmd.getOptionValue("requestQueueUrl");
+        appAMIID = isNotNullOrEmpty(cmd.getOptionValue("appAMIID")) ? cmd.getOptionValue("appAMIID") : "ami-0e355297545de2f82";
+        appInstanceType = isNotNullOrEmpty(cmd.getOptionValue("appInstanceType")) ? cmd.getOptionValue("appInstanceType") : "t2.micro";
+        appSubnetID = isNotNullOrEmpty(cmd.getOptionValue("appSubnetID")) ? cmd.getOptionValue("appSubnetID") : getDefaultSubnet();
+        appSecurityGroups = isNotNullOrEmpty(cmd.getOptionValue("appSecurityGroups")) ? cmd.getOptionValue("appSecurityGroups") : "default";
+        keyName = cmd.getOptionValue("keyName");
+        iamInstanceRole = cmd.getOptionValue("iamInstanceRole");
+        clusterID = cmd.getOptionValue("clusterIdentifier");
+        
         System.out.println( "Autoscaling Controller starts" );
-        /*final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
-        DescribeInstanceStatusRequest describeInstanceRequest = new DescribeInstanceStatusRequest().withInstanceIds("i-001dae1c9d677f004");
-        DescribeInstanceStatusResult describeInstanceResults = ec2.describeInstanceStatus(describeInstanceRequest);
-        DescribeInstancesRequest descReq = new DescribeInstancesRequest().withInstanceIds("i-001dae1c9d677f004");
-        DescribeInstancesResult describeInstanceResult = ec2.describeInstances(descReq);
-        List<InstanceStatus> state = describeInstanceResults.getInstanceStatuses();
-        System.out.println("Desc: "+describeInstanceResult.getReservations().get(0).getInstances().get(0).getState().getName());
-        System.out.println("sate"+ state);
-        if(!state.isEmpty())
-        System.out.println("Desc: "+state.get(0).getInstanceState());*/
         
         while(true) {
         	System.out.println("Recent Instance Status: "+recent_instance_status);
@@ -62,7 +171,7 @@ public class AutoscalingController
 		                DescribeInstanceStatusResult describeInstanceResult = ec2.describeInstanceStatus(describeInstanceRequest);
 		                DescribeInstancesRequest descReq = new DescribeInstancesRequest().withInstanceIds(id);
 		                DescribeInstancesResult InstanceResult = ec2.describeInstances(descReq);
-//		                System.out.println();
+		                
 		                List<InstanceStatus> state = describeInstanceResult.getInstanceStatuses();
 		                System.out.println("Desc: "+InstanceResult.getReservations().get(0).getInstances().get(0).getState().getName());
 		                System.out.println("State of Instances: "+state);
@@ -127,21 +236,19 @@ public class AutoscalingController
 	}
 	
 	public static void checkAndCreateInstance() {
-		System.out.println();
+
 		AutoscalingController obj = new AutoscalingController();
 		
 		int num_of_messages_in_request_queue = 
 				obj.getNumberOfMessagesInQueue(requestQueueUrl);
-		System.out.println("Num of Requests "+num_of_messages_in_request_queue);
 		int num_of_instances = 0;
 		
 		if(num_of_messages_in_request_queue > 0) {
 			
 			num_of_messages_in_request_queue = obj.getNumberOfMessagesInQueue(requestQueueUrl); 
 			if( isScalingRequired(num_of_messages_in_request_queue)) {
-				System.out.println("Scaling Required");
 				num_of_instances = AppTierRunningIns();
-				System.out.println("Number of Instances "+num_of_instances);
+				
 				if(num_of_instances+num_of_messages_in_request_queue < total_instances_allowed) {
 					for(int ins = 0; ins < num_of_messages_in_request_queue; ins++ ) {
 						obj.createInstanceWithUserData();
@@ -218,18 +325,21 @@ public class AutoscalingController
     public synchronized static int AppTierRunningIns() {
     	//just to make the threads a bit slower then each other
 	    final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
-	    String imageId = "ami-0e355297545de2f82"; 
+	    String imageId = appAMIID; 
 	    int minInstanceCount = 1; //create 1 instance
 	    int maxInstanceCount = 1;
 	    DescribeInstancesRequest request = new DescribeInstancesRequest();	
 	    RunInstancesRequest rir = new RunInstancesRequest(imageId,
 	                minInstanceCount, maxInstanceCount);
 	    List<String> valuesT1 = new ArrayList<String>();
-	    valuesT1.add("1");
+	    valuesT1.add(clusterID);
+	    List<String> valuesT2 = new ArrayList<String>();
+	    valuesT2.add("App Server");
 	    System.out.println("List of instances:");
-	    Filter filter1 = new Filter("tag:Stack", valuesT1);
+	    Filter filter1 = new Filter("tag:Cluster", valuesT1);
+	    Filter filter2 = new Filter("tag:Role", valuesT2);
 	    int num_instances = 0;
-	    DescribeInstancesResult res = ec2.describeInstances(request.withFilters(filter1));
+	    DescribeInstancesResult res = ec2.describeInstances(request.withFilters(filter1, filter2));
 	    List<Reservation> reservations = res.getReservations();
 //	    System.out.println("Reservations: "+reservations);
 	    try {
@@ -262,22 +372,44 @@ public class AutoscalingController
     
     public String createInstanceWithUserData() {
     	String str = "";
-    	List<String> sec_grp = new ArrayList<String>();
-    	sec_grp.add("sg-0660bd0fc3dfd47dc");
     	final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
     	
+		List<String> securityGroups = new ArrayList<String>();
+		String[] sgs = appSecurityGroups.split(",");
+		DescribeSecurityGroupsRequest dsr = new DescribeSecurityGroupsRequest();
+		for(int i=0; i<sgs.length;i++) {
+			List<Filter> filters = Arrays.asList(
+					new Filter("group-name",Arrays.asList(sgs[i])));
+			dsr.setFilters(filters);
+			DescribeSecurityGroupsResult result = ec2.describeSecurityGroups(dsr);
+			if(result.getSecurityGroups().size() == 0) {
+				System.err.println("Security group \"" + sgs[i] + "\" not found. Skipping it...");
+				continue;
+			}
+			SecurityGroup s = result.getSecurityGroups().get(0);
+			securityGroups.add(s.getGroupId());
+		}
+		if(securityGroups.size() == 0) {
+			List<Filter> filters = Arrays.asList(
+					new Filter("group-name",Arrays.asList("default")),
+					new Filter("vpc-id",Arrays.asList(getDefaultVPC())));
+			dsr.setFilters(filters);
+			DescribeSecurityGroupsResult result = ec2.describeSecurityGroups(dsr);
+			securityGroups.add(result.getSecurityGroups().get(0).getGroupName());
+		}
+    	
 		System.out.println("create an Instance");
-		String imageId = "ami-0e355297545de2f82";
+		String imageId = appAMIID;
 		int min =1;
 		int max =1;
 		IamInstanceProfileSpecification iam = new IamInstanceProfileSpecification().withName("ProjectInstanceRole");
 		RunInstancesRequest request = new RunInstancesRequest(imageId, min, max);
-		request.setInstanceType("t2.micro");
+		request.setInstanceType(appInstanceType);
 		request.setUserData(getUserDataScript());
-		request.setKeyName("cc_trio");
-		request.setSecurityGroupIds(sec_grp);
+		request.setKeyName(keyName);
+		request.setSecurityGroupIds(securityGroups);
 		request.setIamInstanceProfile(iam);
-		request.setSubnetId("subnet-aea798f5");
+		request.setSubnetId(appSubnetID);
 //		DescribeInstancesRequest request = new DescribeInstancesRequest();	
 //        RunInstancesRequest rir = new RunInstancesRequest(imageId,
 //                min, max);
@@ -294,7 +426,9 @@ public class AutoscalingController
 		str = resultInstance.getInstanceId();
 		CreateTagsRequest createTagsRequest = new CreateTagsRequest()
                 .withResources(resultInstance.getInstanceId())
-                .withTags(new Tag("Stack", "1"));
+                .withTags(new Tag("Cluster", clusterID),
+                		new Tag("Role", "App Server"),
+                		new Tag("Name", clusterID + "-app-server"));
         ec2.createTags(createTagsRequest);
 		
 		resultInstance = result.getReservation().getInstances().get(0);
